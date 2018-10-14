0db57c7ddf657d009e99694673da971ff8f6c671
alemasle <alexislm35@outlook.fr>
2018-10-14 15:59:49 +0200

    SHA3 et midori :'(

--- TP2/Midori.py
+++ TP2/Midori.py
@@ -16,17 +16,15 @@ A11 = [0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0]
 A12 = [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0]
 A13 = [1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0]
 A14 = [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0]
+
 A = [A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14]
 
-
-
 def aes_input(message):
-
     key = gen_key()
-    midori64(message, key)
+    res = midori64(message, key)
+    return res
 
 def gen_key():
-
     return random.getrandbits(128)
 
 def midori64(msg, k):
@@ -49,14 +47,12 @@ def SubCell(s):
         res |= sbox[(s >> 4*i)& 0xf] << 4*i
     return res
 
-
 def ShuffleCell(s):
     p = [0,10,5,15,14,4,11,1,9,3,12,6,7,13,2,8]
     res = 0
     for i in range(16):
         res |= ((s >> 4*p[i])& 0xf) << 4*i
     return res
-
 
 def MixColumns(s):
     res = 0
@@ -69,9 +65,25 @@ def MixColumns(s):
         res |= u << 16*i
     return res
 
-
 def main():
     msg = input("Entrez 64 bits ( int ):\n")
     aes_input(int(msg))
+    # from Midori_imported import MidoriEncrypt
+    # from Midori_imported import MidoriDecrypt
+    #
+    # plaintext = input("Message 32 octets max:\n") # 32 chars
+    #
+    # plainHex = "0x" + plaintext.encode("utf-8").hex() #Transforme la string d'entree en string hexadecimal
+    # key = "0x687ded3b3c85b3f35b1009863e2a8cbf" #Cle prise dans l'autre programme (Aleatoire ?)
+    # round = 20 # Plante au dessus de 20 round
+    #
+    # c = MidoriEncrypt(plainHex, key, round)
+    # p = MidoriDecrypt(c, key, round)
+    #
+    # print("\nPlainHexa:", plainHex)
+    # print("key:", key)
+    # print("cipher:", c)
+    # print("Resultat decryption:", bytes.fromhex(p[2:]).decode('utf-8'))
+
 
 main()
--- TP3/SHA3.py
+++ TP3/SHA3.py
@@ -1,7 +1,22 @@
 #!/bin/env python3
+
+import hashlib
+
+
 r = 0
 c = 0
 lr = 576
+
+RC = [
+  0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+  0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+  0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+  0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+  0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+  0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+  0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+  0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+]
 
 def change_to_be_bin(m='a'):
     x = 0
@@ -31,3 +46,18 @@ def padding(m=m,lr=576):
         mess.append(m[0:lr])
         mess.append(padding(m[lr:],lr))
     return mess
+
+def sha3Hash(string):
+    s = hashlib.sha3_256()
+    s.update(data)
+    dig = s.hexdigest()
+    return dig;
+
+
+def main():
+    data = input("Message to hash sha3-256:\n").encode()
+    sha3Hash(data)
+
+
+
+main()
