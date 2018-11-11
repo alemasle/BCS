9ec4f6687dae0cfeab906674401a36c084c10b1d
alemasle <alexislm35@outlook.fr>
2018-11-11 15:39:13 +0100

    Mode CTR adde

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import random
-from sys import argv
 import argparse
-from Midori64 import midori, antiMidori
+from sys import argv
+from Midori64 import midori
+from SHA3 import sha3Hash
 
 def decoupe_blocks(data, size):
     data_size = len(data)
@@ -15,7 +16,7 @@ def decoupe_blocks(data, size):
         list_block.append(data[i: i+size])
 
     while len(list_block[-1]) < size:
-        list_block[-1] = '0' + list_block[-1]
+        list_block[-1] = list_block[-1] + '0'
 
     return list_block
 
@@ -38,9 +39,34 @@ def CBC(iv, blocks, key, size):
     print(list_chiffre)
     return list_chiffre
 
-def chiffrage(msg, key): # Midori a faire
+def chiffrage(msg, key): # Midori a utiliser
     return msg ^ key
 
+def array_to_hex(li):
+    res = ""
+    for c in li:
+        res += hex(c)[2:]
+    return res
+
+def int_to_hex(i):
+    h = hex(i)[2:]
+    while len(h) < 8:
+        h = '0' + h
+
+    return h
+
+def CTR(nonce, blocks, key):
+    ctr = 0
+    nonce_counter = string_to_hex(nonce + int_to_hex(ctr))
+    list_ret = []
+    for b in blocks:
+        ret = midori(nonce_counter, key)
+        res = array_to_hex(ret)
+        list_ret.append(hex(int(res, 16) ^ int(b, 16))[2:])
+        ctr += 1
+        nonce_counter = string_to_hex(nonce + int_to_hex(ctr))
+
+    return list_ret
 
 def string_to_bytes(m='a'):
     li = ""
@@ -51,24 +77,50 @@ def string_to_bytes(m='a'):
         li += tmp
     return li
 
-def main():
+def string_to_hex(m):
+    res = []
+    for c in m:
+        tmp = '0x' + c
+        res.append(int(tmp, 16))
+    return res
 
+def generate_key(key):
+    hash = sha3Hash(key)
+
+    tmp = int(hash[0:32], 16)
+    tmp2 = int(hash[32:64], 16)
+    return hex(tmp ^ tmp2)[2:]
+
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("message", help="Le message a chiffrer")
-    parser.add_argument("key", help="La clé de chiffrement (Message)")
+    parser.add_argument("key", help="La clé de chiffrement (hexa)")
     args = parser.parse_args()
 
-    size_block = 16
-    msg = string_to_bytes(args.message)
-    key = string_to_bytes(args.key)
+    size_block = 16 #16 en char hexa soit 8 octets --> 64 bits
+
+    msg = args.message.encode().hex()
+    key = generate_key(args.key.encode())
+
     li = decoupe_blocks(msg, size_block)
-    iv = bin(random.getrandbits(size_block))[2:]
+    iv = hex(random.getrandbits(size_block * 6))[2:] # Nonce taille 24 hexa pour atteindre 32 char hexa
 
-    print("message:", args.message, "\nclef:", args.key)
-    # print("message:", msg, "\niv:", iv, "\nkey:", key)
-    # print("block decoupes:", li)
+    ke = string_to_hex(key)
 
-    CBC(iv, li, key, size_block)
+    res = CTR(iv, li, ke) # Chiffrement
+    print("chiffre:", "".join(res))
+
+
+    cl = CTR(iv, res, ke) # Dechiffrement
+
+    final = ""
+    for b in cl:
+        final += b
+    
+    print("dechiffre:", bytes.fromhex(final).decode())
+
 
 if __name__ == '__main__':
     main()
